0e1d3a6a0d840ee90186db6e7ed8b0f47dc6ebb9
William <c14180054@john.petra.ac.id>
2021-04-17 20:27:23 +0700

    Third

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
             loadChildren: () => import('./offers/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
           },
           {
-            path:'placeId',
+            path:':placeId',
             loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
           }
         ]
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -15,4 +15,9 @@ export class PlacesService {
     return [...this._places];
   }
   constructor() { }
+  getPlace(id:string){
+    return {...this._places.find(
+      p => p.id === id
+    )}
+  }
 }
